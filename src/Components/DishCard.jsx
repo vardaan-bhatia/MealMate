021e93d0cc69800e5a6539bd0dbb5a30c7f57355
Vardaan Bhatia <vardaanbhatia55@gmail.com>
2024-06-07 02:48:26 +0530

    cuisine render

--- src/Components/DishCard.jsx
+++ src/Components/DishCard.jsx
@@ -2,35 +2,12 @@ import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const dishCategories = [
-  "Chinese",
-  "Indian",
-  "Italian",
-  "Mexican",
-  "Thai",
-  "Rasgulla",
-  "Pizza",
-  "Burger",
-  "Pav Bhaji",
-  "Pasta",
-  "Tacos",
-  "Sushi",
-];
-
-const DishCard = () => {
+const DishCard = ({ boxes }) => {
+  const { imageId, accessibility } = boxes.info;
+  const image = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/${imageId}`;
   return (
-    <div className="feedtop">
-      <div className="Mindtop">
-        <h1 className="mindh1">What's on your mind?</h1>
-        <div className="round-container">
-          {dishCategories.map((category, index) => (
-            <div className="round-pair" key={index}>
-              <div className="round"></div>
-              <p>{category}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+    <div className="mind-item">
+      <img src={image} alt={accessibility.altText} />
     </div>
   );
 };
--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -7,12 +7,26 @@ import mind from "../utils/mindobj";
 const Feed = () => {
   return (
     <>
-      <DishCard />
+      <h1 className="mind-heading">What's on your mind?</h1>
+      <div className="mind-container">
+        {mind.map((e) =>
+          e.info && e.info.id ? (
+            <DishCard boxes={e} key={e.info.id} />
+          ) : (
+            console.error("Invalid object structure in mind array:", e)
+          )
+        )}
+      </div>
+
       <h1 className="f1main">Top restaurant chains in Patiala</h1>
       <div className="fcontainer">
-        {resobj.map((index) => (
-          <FoodCard restaurant={index} key={index.info.id} />
-        ))}
+        {resobj.map((index) =>
+          index.info && index.info.id ? (
+            <FoodCard restaurant={index} key={index.info.id} />
+          ) : (
+            console.error("Invalid object structure in resobj array:", index)
+          )
+        )}
       </div>
     </>
   );
