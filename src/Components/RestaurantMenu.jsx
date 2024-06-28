8807823ccb9545d9561fd8557cb2f6928fbd035f
Vardaan Bhatia <vardaanbhatia55@gmail.com>
2024-06-28 06:44:14 +0530

    menu

--- src/Components/RestaurantMenu.jsx
+++ src/Components/RestaurantMenu.jsx
@@ -4,8 +4,8 @@ import "../CSS/RestaurantMenu.css";
 import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () => {
-  const [menuItems, setMenuItems] = useState([]);
-  const [menuItems2, setMenuItems2] = useState([]);
+  const [menuItems, setMenuItems] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     MenuFetch();
@@ -13,40 +13,38 @@ const RestaurantMenu = () => {
 
   const MenuFetch = async () => {
     try {
-      const response = await axios.get(process.env.REACT_APP_MENU);
-      const datachain =
-        response?.data?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR
-          ?.cards[2]?.card?.card?.itemCards || [];
-      setMenuItems(datachain);
-      const datachain2 =
-        response?.data?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR
-          ?.cards[3]?.card?.card?.itemCards || [];
-      setMenuItems2(datachain2);
+      const response = await axios.get(
+        "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.73390&lng=76.78890&restaurantId=770885"
+      );
+      setMenuItems(response.data.data);
     } catch (error) {
       console.log("error hai", error);
-      return error;
+    } finally {
+      setLoading(false);
     }
   };
+
+  // Safely access itemCards
+  const itemCards =
+    menuItems?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+      ?.card?.itemCards || [];
+
   return (
     <>
       <div className="resmenutop">
-        {menuItems.length === 0 || menuItems2.length === 0 ? (
+        {loading ? (
           <Shimmer />
         ) : (
-          <>
-            <ul>
-              {menuItems.map((e) => (
-                <li key={e?.card?.info?.id}>{e?.card?.info?.name || ""}</li>
-              ))}
-              <b>{"INDIAN"}</b>
-              {menuItems2.map((f) => (
-                <li key={f?.card?.info?.id}>{f?.card?.info?.name || ""}</li>
-              ))}
-            </ul>
-          </>
+          <ul>
+            {itemCards.map((item) => (
+              <li key={item?.card?.info?.id}>{item?.card?.info?.name || ""}</li>
+            ))}
+            <b>{"INDIAN"}</b>
+          </ul>
         )}
       </div>
     </>
   );
 };
+
 export default RestaurantMenu;
